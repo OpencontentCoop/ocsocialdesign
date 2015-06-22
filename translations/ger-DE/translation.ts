994933bc04811828851f22e8c13dd2f04604a03b
Luca Realdi <luca.realdi@opencontent.it>
2015-06-22 12:05:46 +0200

    ger-DE

---
+++ translations/ger-DE/translation.ts
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>ocsocialdesign</name>
+    <message>
+        <source>I cookie ci aiutano ad erogare servizi di qualità. Utilizzando i nostri servizi, l&apos;utente accetta le nostre modalità d&apos;uso dei cookie.</source>
+        <translation>Cookies helfen uns, die Qualität der Dienstleistungen zu liefern. Nutzung unserer Dienste erklären Sie sich mit unserer Art der Verwendung von Cookies.</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <source>Maggiori informazioni</source>
+        <translation>mehr Informationen</translation>
+    </message>
+    <message>
+        <source>Contatti</source>
+        <translation>Kontakte</translation>
+    </message>
+</context>
+<context>
+    <name>ocsocialdesign/menu</name>
+    <message>
+        <source>Accedi</source>
+        <translation>Einloggen</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/config</name>
+    <message>
+        <source>Aggiungi</source>
+        <translation>hinzufügen</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/dashboard</name>
+    <message>
+        <source>Annulla</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <source>Elimina gli elementi</source>
+        <translation>Löscht Artikel</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/signin</name>
+    <message>
+        <source>Login</source>
+        <translation>Einloggen</translation>
+    </message>
+    <message>
+        <source>Could not login</source>
+        <translation>Konnte nicht einloggen</translation>
+    </message>
+    <message>
+        <source>A valid username and password is required to login.</source>
+        <translation>Ein gültiger Benutzername und Passwort ist erforderlich, um sich anzumelden.</translation>
+    </message>
+    <message>
+        <source>Access not allowed</source>
+        <translation>Zugriff nicht erlaubt</translation>
+    </message>
+    <message>
+        <source>You are not allowed to access %1.</source>
+        <translation>Sie sind nicht zugreifen darf %1</translation>
+    </message>
+    <message>
+        <source>Indirizzo Email</source>
+        <translation>Email Adresse</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Remember me</source>
+        <translation>Erinnere dich an mich</translation>
+    </message>
+    <message>
+        <source>Accedi</source>
+        <translation>Einloggen</translation>
+    </message>
+    <message>
+        <source>Hai dimenticato la password?</source>
+        <translation>Password vergessen?</translation>
+    </message>
+</context>
+<context>
+    <name>social_user/user_edit</name>
+    <message>
+        <source>Profilo utente</source>
+        <translation>Benutzerprofil</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Benutzername</translation>
+    </message>
+    <message>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <source>Modifica profilo</source>
+        <translation>Benutzerprofil erstellen</translation>
+    </message>
+    <message>
+        <source>Cambia la password</source>
+        <translation>Passwort ändern</translation>
+    </message>
+</context>
+</TS>
