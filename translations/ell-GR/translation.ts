7d599a5a8fb2ce1d8876d28cd7f9b2a1ae7f22f9
Luca Realdi <lr@opencontent.it>
2019-12-16 15:33:08 +0100

    Aggiunge il file di traduzione delle stringhe in greco

---
+++ translations/ell-GR/translation.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+  <context>
+    <name>ocsocialdesign</name>
+    <message>
+      <source>Cookies help us to deliver quality services. By using our services, you agree to our use of cookies mode.</source>
+      <translation>Τα cookies μας βοηθούν να προσφέρει υπηρεσίες υψηλής ποιότητας. Χρησιμοποιώντας τις υπηρεσίες μας, συμφωνείτε με τη χρήση των cookies λειτουργία.</translation>
+    </message>
+    <message>
+      <source>OK</source>
+      <translation>Εντάξει</translation>
+    </message>
+    <message>
+      <source>More information</source>
+      <translation>Περισσότερες πληροφορίες</translation>
+    </message>
+    <message>
+      <source>Contacts</source>
+      <translation>Επαφές</translation>
+    </message>
+  </context>
+  <context>
+    <name>ocsocialdesign/menu</name>
+    <message>
+      <source>Login</source>
+      <translation>Σύνδεση</translation>
+    </message>
+  </context>
+</TS>
